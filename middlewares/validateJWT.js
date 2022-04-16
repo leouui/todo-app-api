6d665f15d2ba4f8e34bcc2468980176df7b725ce
Leonardo Moreno <leoui.moreno@gmail.com>
2022-04-16 17:09:43 -0500

    removing consolelog from validateJWT middleware

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -2,10 +2,9 @@ const jwt = require('jsonwebtoken')
 
 const validateJWT = (req,res,next) => {
     const token = req.headers["x-token"]
-    console.log(token);
+
     try {
         const {uid,exp} = jwt.verify(token,process.env.SECRET_KEY,)
-        console.log(uid,exp);
         
         req.userID = uid
         req.expiration = exp
